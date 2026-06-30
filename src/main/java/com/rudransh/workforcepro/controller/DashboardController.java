751e2d7f9b22d3051c9832cdc77fe869ed712cd5
Rudransh rai <rudranshrai08@gmail.com>
2026-06-30 21:50:05 +0530

    Added Dashboard module

---
+++ src/main/java/com/rudransh/workforcepro/controller/DashboardController.java
@@ -0,0 +1,47 @@
+package com.rudransh.workforcepro.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rudransh.workforcepro.repository.AttendanceRepository;
+import com.rudransh.workforcepro.repository.DepartmentRepository;
+import com.rudransh.workforcepro.repository.EmployeeRepository;
+import com.rudransh.workforcepro.repository.LeaveRepository;
+import com.rudransh.workforcepro.repository.PayrollRepository;
+
+@RestController
+public class DashboardController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private LeaveRepository leaveRepository;
+
+    @Autowired
+    private AttendanceRepository attendanceRepository;
+
+    @Autowired
+    private PayrollRepository payrollRepository;
+
+    @GetMapping("/api/dashboard")
+    public Map<String, Long> getDashboardData() {
+
+        Map<String, Long> dashboard = new HashMap<>();
+
+        dashboard.put("Total Employees", employeeRepository.count());
+        dashboard.put("Total Departments", departmentRepository.count());
+        dashboard.put("Total Leaves", leaveRepository.count());
+        dashboard.put("Total Attendance", attendanceRepository.count());
+        dashboard.put("Total Payroll", payrollRepository.count());
+
+        return dashboard;
+    }
+}
