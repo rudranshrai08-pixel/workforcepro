995ca4c9a64a75c1825876e2d535688d3892c37d
Rudransh rai <rudranshrai08@gmail.com>
2026-07-06 16:21:20 +0530

    Initialized React frontend and project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#0f172a",
+        minHeight: "100vh",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>🚀 WorkforcePro Frontend Started Successfully</h1>
+    </div>
+  );
+}
+
+export default App;
